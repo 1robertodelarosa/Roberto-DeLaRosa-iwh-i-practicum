b77f584d306f0d06f8a3bc736731219b2343b593
rdelarosa@amherst.com <1robertodelarosa@gmail.com>
2024-01-03 09:19:40 -0600

    Final done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-
+const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -31,18 +31,7 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 app.get('/update-cobj', async (req, res) => {
-    const animals = 'https://api.hubapi.com/crm/v3/objects/2-21825197?properties=animal_name%2Canimal_type%2Caverage_weight%2Caverage_length';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(animals, { headers });
-        const data = resp.data.results;
-        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });      
-    } catch (error) {
-        console.error(error);
-    }
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });      
 });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
@@ -69,7 +58,6 @@ app.post('/update-cobj', async (req, res) => {
     } catch(err) {
         console.error(err);
     }
-
 });
 
 // * Localhost
